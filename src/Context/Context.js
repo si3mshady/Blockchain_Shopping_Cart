6e304f40ce5446463037487eb57ee9cc2fc8564e
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-28 00:56:26 -0500

    save point

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,25 +1,39 @@
-import { createContext } from "react"
-
+import { useReducer,createContext, useContext } from "react"
+import {cartReducer} from './Reducer'
 import {data} from './MasterWineList.js'
+
 const Cart = createContext();
 
 
 
-const Context = ({children}) => {  const results = data.map(e => (
+const Context = ({children}) => 
+
+{  const results = data.map(e => (
     {
      name: e.Name,
      image: e.URL,
      price: e.Price,     
      ratings: e.Rating,
-    
+     year: e.Year    
 
     }
   ));
 
-  console.log(results)
+  // console.log(results)
 
-  return <Cart.Provider>{children}</Cart.Provider>
+
+
+  const [state,dispatch] = useReducer(cartReducer, {
+    products: results,
+    cart: []
+  })
+
+  return <Cart.Provider value={{state,dispatch}}>{children}</Cart.Provider>
 
 }
 
 export default Context
+export const CartState = () => {
+  return useContext(Cart);
+}
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
+import { CartState } from "../../Context/Context"
+
 const Home = () => {
-
+  const { state: {products} } = CartState();
+  console.log(products)
   return (
-    <div>
+    <div className="home">
+        {/* Filters */}
 
-        Home
+        <div className="productContainer">
+              { 
+                products.map((e,i) => {
+                  return <div> {e.name} </div>
+                })
+              }
+        </div>
 
     </div>
   )
